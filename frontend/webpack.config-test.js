93579cf94c36d395d6fccc62ad48705f56a2dcc5
Tkachenko Vladislav <webkadiz@gmail.com>
2020-06-29 23:34:00 +0300

    webpack config for testing on mocha

---
+++ frontend/webpack.config-test.js
@@ -0,0 +1,12 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  output: {
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+  },
+  devtool: 'inline-cheap-module-source-map',
+  target: 'node', // webpack should compile node compatible code
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder,
+  mode: 'development'
+};
