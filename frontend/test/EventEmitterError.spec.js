32aec7e63e8bcbfa62312718d470da390c0a69b0
Tkachenko Vladislav <webkadiz@gmail.com>
2020-06-30 15:28:40 +0300

    tests for EventEmitterError

---
+++ frontend/test/EventEmitterError.spec.js
@@ -0,0 +1,19 @@
+import {assert} from 'chai'
+import EventEmitterError from '../src/first-stage/classes/EventEmitterError'
+
+describe("EventEmitterError", function() {
+  it("Instance of", function() {
+    const eventEmitterError = new EventEmitterError()
+    assert.instanceOf(eventEmitterError, EventEmitterError)
+  })
+
+  it("Name error ", function() {
+    const eventEmitterError = new EventEmitterError()
+    assert.equal(eventEmitterError.name, "EventEmitterError")
+  })
+
+  it("Message right ", function() {
+    const eventEmitterError = new EventEmitterError("emit wrong")
+    assert.equal(eventEmitterError.message, "emit wrong")
+  })
+})
