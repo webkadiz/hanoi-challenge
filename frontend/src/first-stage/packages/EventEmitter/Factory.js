1879e13ef07ca2d0d760534bda89f2b22bba687d
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-02 13:01:58 +0300

    add _createDeps

--- frontend/src/first-stage/packages/EventEmitter/Factory.js
+++ frontend/src/first-stage/packages/EventEmitter/Factory.js
@@ -1,16 +1,29 @@
 export default class Facroty {
   constructor(classToCreate, ...deps) {
     this._classToCreate = classToCreate
-    this.deps = deps
+    this._deps = deps
   }
 
   create() {
-    const createdDeps = []
-    for (const dependency of this._depends) {
-      const createdDependency = new dependency()
-      createdDeps.push(createdDependency)
-    }
+    const createdDeps = this._createDeps()
 
     return new this._classToCreate(...createdDeps)
+  }
+
+  _createDeps() {
+    const createdDeps = []
+    for (const dependency of this._deps) {
+      let createdDependency
+
+      if (dependency.prototype === undefined) {
+        createdDependency = dependency
+      } else {
+        createdDependency = new dependency()
+      }
+
+      createdDeps.push(createdDependency)
+    }
+    
+    return createdDeps
   }
 }
