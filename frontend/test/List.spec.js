4dc5f73d5e78e89b69779d6e25a4c8749eb30009
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-02 17:46:33 +0300

    refactor List interface in tests

--- frontend/test/List.spec.js
+++ frontend/test/List.spec.js
@@ -4,22 +4,22 @@ import List from '../src/first-stage/packages/EventEmitter/List'
 describe("List", function() {
   describe("Creation", function() {
     it("Create without init", function() {
-      new List().create()
+      new List()
     })
 
     it("Create with right init", function() {
-      new List().create([1, 2, 3])
+      new List([1, 2, 3])
     })
 
     it("Create with wrong init", function() {
       try {
-        new List().create("123")
+        new List("123")
       } catch {return}
       assert.fail()
     })
 
     it("Get init", function() {
-      const list = new List().create([1, 2, 3])
+      const list = new List([1, 2, 3])
       debugger
       assert.deepEqual(list.get(2), [2])
     })
@@ -27,18 +27,18 @@ describe("List", function() {
 
   describe("Add And Get", function() {
     it("get do not exists element", function() {
-      const list = new List().create()
+      const list = new List()
       assert.deepEqual(list.get(2), false)
     })
 
     it("One equal element", function() {
-      const list = new List().create()
+      const list = new List()
       list.add(3)
       assert.deepEqual(list.get(3), [3])
     })
 
     it("Two equal element", function() {
-      const list = new List().create()
+      const list = new List()
       list.add(3)
       list.add(3)
       list.add(1)
@@ -46,7 +46,7 @@ describe("List", function() {
     })
 
     it("Get All", function() {
-      const list = new List().create()
+      const list = new List()
       list.add(3)
       list.add(3)
       list.add(1)
@@ -54,7 +54,7 @@ describe("List", function() {
     })
 
     it("Chain add", function() {
-      const list = new List().create()
+      const list = new List()
       list.add(2).add(3).add(4)
       assert.deepEqual(list.getAll(), [2, 3, 4])
     })
@@ -62,25 +62,25 @@ describe("List", function() {
 
   describe("Remove and clear", function() {
     it("Remove don't exists element", function() {
-      const list = new List().create([1, 2, 3])
+      const list = new List([1, 2, 3])
       list.remove(4)
       assert.deepEqual(list.getAll(), [1, 2, 3])
     })
 
     it("Remove one equal element", function() {
-      const list = new List().create([1, 2, 3])
+      const list = new List([1, 2, 3])
       list.remove(2)
       assert.deepEqual(list.getAll(), [1, 3])
     })
 
     it("Remove Two equal element", function() {
-      const list = new List().create([1, 2, 3, 2])
+      const list = new List([1, 2, 3, 2])
       list.remove(2)
       assert.deepEqual(list.getAll(), [1, 3])
     })
 
     it("Clear", function() {
-      const list = new List().create([1, 2, 3, 2])
+      const list = new List([1, 2, 3, 2])
       list.clear()
       assert.deepEqual(list.getAll(), [])
     })
@@ -88,12 +88,12 @@ describe("List", function() {
 
   describe("Length", function() {
     it("from create", function() {
-      const list = new List().create([1, 2, 3])
+      const list = new List([1, 2, 3])
       assert.equal(list.length(), 3)
     })
 
     it("from add", function() {
-      const list = new List().create()
+      const list = new List()
       list.add(2)
       assert.equal(list.length(), 1)
     })
