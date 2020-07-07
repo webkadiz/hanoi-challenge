aae74d309437a419c13567a84550697e1d214feb
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-07 23:26:00 +0300

    create GameManager

---
+++ frontend/src/first-stage/classes/GameManager.js
@@ -0,0 +1,57 @@
+export default class GameManager {
+  constructor(game, socketManager, videoStreamManager, snowfall, emitter) {
+    this.game = game
+    this.socketManager = socketManager
+    this.videoStreamManager = videoStreamManager
+    this.emitter = emitter
+    this.snowfall = snowfall
+  }
+
+  run() {
+    this.emitter.on('socketOpen', this.socketOpen.bind(this))
+    this.emitter.on('socketMessage', this.socketMessage.bind(this))
+    this.emitter.on('socketClose', this.socketClose.bind(this))
+    this.emitter.on('videoStreamDataAvailable', this.videoStreamDataAvailable.bind(this))
+    
+    this.socketManager.setHandlers()
+
+    this.game.setGameLevelHandler()
+    this.game.createLevelScreen()
+
+    this.snowfall.init()
+    this.snowfall.animate()
+  }
+
+  socketOpen() {
+    this.socketManager.send(JSON.stringify({
+      create: true
+    }))
+    this.videoStreamManager.init()
+  }
+
+  socketMessage(incomingData) {
+    console.log('income', incomingData)
+    if (incomingData.reload) {
+
+      location.reload()
+    
+    } else if (incomingData.timer === 'start') {
+  
+      this.game.startGame()
+        
+    } else if (incomingData.timer === 'stop') {
+  
+      this.game.stopGame()
+  
+    }
+  }
+
+  socketClose() {
+    this.emitter.off('videoStreamDataAvailable')
+    this.game.setOfflineMode()
+  }
+
+  videoStreamDataAvailable(data) {
+    this.socketManager.send(data)
+  }
+}
