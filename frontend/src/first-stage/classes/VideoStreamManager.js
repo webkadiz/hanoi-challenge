20be3d3a9c2a8b3d6fa323a8c7b4137a7ee551db
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-07 23:26:32 +0300

    create VideoStreamManager

---
+++ frontend/src/first-stage/classes/VideoStreamManager.js
@@ -0,0 +1,26 @@
+export default class VideoStreamManager {
+  constructor(emitter) {
+    this.emitter = emitter
+  }
+
+  init() {
+    navigator.mediaDevices.getDisplayMedia({ video: { width: 640, height: 360, frameRate: 10} })
+      .then(stream => {
+        const recorder = new MediaRecorder(stream)
+        
+        recorder.ondataavailable = e => {
+          if (this.emitter.listenersCount('videoStreamDataAvailable')) {
+            this.emitter.emit('videoStreamDataAvailable', e.data)
+          } else {
+            recorder.ondataavailable = null
+            recorder.stop()
+          }
+        }
+        
+        recorder.start(1000)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
