ada2aa644859a8538620d5176afa6666ebdecd1f
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-11 20:21:25 +0300

    add resolve @

--- frontend/webpack.config-test.js
+++ frontend/webpack.config-test.js
@@ -5,6 +5,11 @@ module.exports = {
     devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
   },
+  resolve: {
+		alias: {
+			'@': __dirname + '/src'
+		}
+	},
   devtool: 'inline-cheap-module-source-map',
   target: 'node', // webpack should compile node compatible code
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder,
