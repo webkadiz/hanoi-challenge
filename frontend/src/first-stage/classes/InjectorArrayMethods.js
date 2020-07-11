bc0c8813c0e8bf1415584a7c9a278ba9b47dd812
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-11 20:22:12 +0300

    InjectorArrayMethods implementation

---
+++ frontend/src/first-stage/classes/InjectorArrayMethods.js
@@ -0,0 +1,28 @@
+import { range } from '@/common/utils'
+
+export default class InjectorArrayMethods {
+  inject() {
+    Object.defineProperty(Array.prototype, 'shiftLeft', {
+      value() {
+    
+        for (const i of range(1, this.length)) 
+          this[i - 1] = this[i]
+        
+        this[this.length - 1] = 0
+        
+        return this
+      }
+    })
+    
+    Object.defineProperty(Array.prototype, 'shiftRight', {
+      value() {
+        for (const i of range(this.length - 2, -1, -1))
+          this[i + 1] = this[i]
+        
+        this[0] = 0
+        
+        return this
+      }
+    })
+  }
+}
