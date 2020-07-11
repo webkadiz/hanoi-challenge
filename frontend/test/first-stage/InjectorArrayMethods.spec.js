3f0e80dd7771772aff300931ea3b5c53eb2b033c
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-11 20:22:28 +0300

    InjectorArrayMethods spec

---
+++ frontend/test/first-stage/InjectorArrayMethods.spec.js
@@ -0,0 +1,18 @@
+import {assert} from 'chai'
+import InjectorArrayMethods from '@/first-stage/classes/InjectorArrayMethods'
+
+describe('InjectorArrayMethods', () => {
+  const injectorArrayMethods = new InjectorArrayMethods()
+  injectorArrayMethods.inject()
+
+  it('inject functions', () => {
+    assert.isFunction([].shiftLeft)
+    assert.isFunction([].shiftRight)
+  })
+
+  it('test work of injected functions', () => {
+    debugger
+    assert.deepEqual([1, 2, 3].shiftLeft(), [2, 3, 0])
+    assert.deepEqual([1, 2, 3].shiftRight(), [0, 1, 2])
+  })
+})
