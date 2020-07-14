0a38d5d5fce7714842e244357a757fd85af724ed
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-14 22:47:56 +0300

    Add ServerEventsProvider

---
+++ frontend/src/last-stage/modules/ServerEventsProvider.js
@@ -0,0 +1,29 @@
+export default class ServerEventsProvider {
+  constructor(socketManager, emitter) {
+    this.socketManager = socketManager
+    this.emitter = emitter
+  }
+
+  launch() {
+    this.socketManager.setHandlers()
+
+    this.emitter.on('socketMessage', (incomingData => {
+      if (incomingData.buffer) {
+        this.emitter.emit('appendBuffer', {
+          buffer: incomingData.buffer,
+          clientIndex: incomingData.clientIndex
+        })
+      } else if(incomingData.data.create) {
+        this.emitter.emit('createMediaSource', {
+          clientIndex: incomingData.clientIndex
+        })        
+      } else if(incomingData.data.gameOver) {
+        this.emitter.emit('handleFirstStageGameOver', {
+          gameOver: incomingData.data.gameOver,
+          additionalScore: incomingData.data.additionalScore,
+          clientIndex: incomingData.clientIndex
+        })    
+      }    
+    }).bind(this))
+  }
+}
