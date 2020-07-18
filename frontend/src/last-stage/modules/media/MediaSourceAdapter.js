9eb52672c9960e3074aac6941775be5c50f69f34
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-19 00:00:09 +0300

    Add implementation MediaSourceAdapter

---
+++ frontend/src/last-stage/modules/media/MediaSourceAdapter.js
@@ -0,0 +1,22 @@
+export default class MediaSourceAdapter {
+  constructor(mediaSource, emitter) {
+    this.mediaSource = mediaSource
+    this.emitter = emitter
+    this.mimeCodec = "video/webm;codecs=vp8"
+  }
+
+  setSourceOpenHandler() {
+    this.mediaSource.addEventListener('sourceopen', () => {
+      const sourceBuffer = this.mediaSource.addSourceBuffer(this.mimeCodec)
+      this.emitter.emit('sourceOpen', sourceBuffer)
+    })
+  }
+
+  getOriginal() {
+    return this.mediaSource
+  }
+
+  getState() {
+    return this.mediaSource.readyState
+  }
+}
