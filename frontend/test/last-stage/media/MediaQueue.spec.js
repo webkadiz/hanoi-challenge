0e6cd94357d5e2077781450ab423180c4b8a890f
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-19 00:17:34 +0300

    Add spec MediaQueue

---
+++ frontend/test/last-stage/media/MediaQueue.spec.js
@@ -0,0 +1,77 @@
+import { assert } from 'chai'
+import sinon from 'sinon'
+import MediaQueue from '../../../src/last-stage/modules/media/MediaQueue'
+
+describe("MediaQueue ", () => {
+
+
+  it("Standard flow", () => {
+    // Arrange
+    const mediaQueue = new MediaQueue()
+
+    // Act
+    mediaQueue.enqueue(1)
+    mediaQueue.enqueue(2)
+    
+    // Assert 
+    assert.equal(mediaQueue.dequeue(), 1)
+  })
+
+  it("dequeuq without enqueue before", () => {
+    // Arrange
+    const mediaQueue = new MediaQueue()
+    const dequeueSpy = sinon.spy(mediaQueue, 'dequeue')
+
+    // Act
+    mediaQueue.dequeue()
+
+    // Assert
+    assert.ok(dequeueSpy.threw("Error"))
+  })
+
+  it("isEmpty - true", () => {
+    // Arrange
+    const mediaQueue = new MediaQueue()
+    
+    // Act
+    const isEmpty = mediaQueue.isEmpty()
+
+    // Assert
+    assert.isTrue(isEmpty)
+  })
+
+  it("isEmpty - false", () => {
+    // Arrange
+    const mediaQueue = new MediaQueue()
+    
+    // Act
+    mediaQueue.enqueue(1)
+    const isEmpty = mediaQueue.isEmpty()
+
+    // Assert
+    assert.isFalse(isEmpty)
+  })
+
+  it("isNotEmpty - false", () => {
+    // Arrange
+    const mediaQueue = new MediaQueue()
+    
+    // Act
+    const isNotEmpty = mediaQueue.isNotEmpty()
+
+    // Assert
+    assert.isFalse(isNotEmpty)
+  })
+
+  it("isNotEmpty - true", () => {
+    // Arrange
+    const mediaQueue = new MediaQueue()
+    
+    // Act
+    mediaQueue.enqueue(1)
+    const isNotEmpty = mediaQueue.isNotEmpty()
+
+    // Assert
+    assert.isTrue(isNotEmpty)
+  })
+})
