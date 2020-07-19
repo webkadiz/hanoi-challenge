dee3a9f9c8e0ec54c1001c07bc401f96b3c57cf8
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-19 12:49:28 +0300

    Add implementation MediaQueue

---
+++ frontend/src/last-stage/modules/media/MediaQueue.js
@@ -0,0 +1,23 @@
+export default class MediaQueue {
+  constructor() {
+    this._queue = []
+  }
+
+  enqueue(item) {
+    this._queue.push(item)
+  }
+  
+  dequeue(item) {
+    if (this.isEmpty()) throw new Error('queue is empty')
+
+    return this._queue.shift()
+  }
+
+  isEmpty() {
+    return this._queue.length === 0
+  }
+
+  isNotEmpty() {
+    return !this.isEmpty()
+  }
+}
