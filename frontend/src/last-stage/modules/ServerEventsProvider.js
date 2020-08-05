1e2857d42369735c2f8c3b85a6b9db9aed7807f5
Tkachenko Vladislav <webkadiz@gmail.com>
2020-08-05 23:20:02 +0300

    Change spec data exchange between server and last stage

--- frontend/src/last-stage/modules/ServerEventsProvider.js
+++ frontend/src/last-stage/modules/ServerEventsProvider.js
@@ -10,16 +10,18 @@ export default class ServerEventsProvider {
     this.emitter.on(
       "socketMessage",
       ((incomingData) => {
-        if (incomingData.buffer) {
+        if (incomingData === "reload") {
+          this.emitter.emit("reloadStage")
+        } else if (incomingData.buffer) {
           this.emitter.emit("appendBuffer", {
             buffer: incomingData.buffer.data,
             clientIndex: incomingData.clientIndex,
           })
-        } else if (incomingData.data.create) {
+        } else if (incomingData.data && incomingData.data.create) {
           this.emitter.emit("createMediaSource", {
             clientIndex: incomingData.clientIndex,
           })
-        } else if (incomingData.data.gameOver) {
+        } else if (incomingData.data && incomingData.data.gameOver) {
           this.emitter.emit("handleFirstStageGameOver", {
             gameOver: incomingData.data.gameOver,
             additionalScore: incomingData.data.additionalScore,
